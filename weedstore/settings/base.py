dfbc59edb7a67936de0d62dea8c858999eaa0461
G.Mekolle <sone_gillis@yahoo.com>
2020-09-07 22:56:42 +0100

    commit

--- weedstore/settings/base.py
+++ weedstore/settings/base.py
@@ -13,20 +13,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'h-)&jg^mri$lf2*m!%6q-377=xzd_&zz!j6fwwiv_y-dhk(7l6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
---
+++ weedstore/settings/production.py
@@ -0,0 +1,5 @@
+from weedstore.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = []
